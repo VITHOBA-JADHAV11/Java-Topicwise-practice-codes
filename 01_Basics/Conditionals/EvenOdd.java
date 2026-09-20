73e90b8c3e4af7de6413f643634d45e95d7eec29
Vithuu <work.gopal01@gmail.com>
2026-09-20 21:04:03 +0530

    Update EvenOdd.java

--- 01_Basics/Conditionals/EvenOdd.java
+++ 01_Basics/Conditionals/EvenOdd.java
@@ -1,7 +1,9 @@
-// Problem: EvenOdd
+/**
+ * EvenOdd: working Java practice program.
+ * Extend this solution with Scanner input, edge-case tests, and complexity analysis.
+ */
 public class EvenOdd {
     public static void main(String[] args) {
-        int n=17; 
-        System.out.println(n%2==0?"Even":"Odd");
+        int n=17; System.out.println(n%2==0?"Even":"Odd");
     }
 }
