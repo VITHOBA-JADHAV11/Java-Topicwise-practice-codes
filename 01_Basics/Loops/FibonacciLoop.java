456f71f1fd0c8d9d09dcfad397e6e84827c3a5ad
Vithuu <work.gopal01@gmail.com>
2026-09-20 21:07:07 +0530

    Update FibonacciLoop.java

--- 01_Basics/Loops/FibonacciLoop.java
+++ 01_Basics/Loops/FibonacciLoop.java
@@ -1,4 +1,12 @@
 /**
+ * FibonacciLoop: working Java practice program.
+ * Extend this solution with Scanner input, edge-case tests, and complexity analysis.
+ */
+public class FibonacciLoop {
+    public static void main(String[] args) {
+        int a=0,b=1; for(int i=0;i<10;i++){System.out.print(a+" "); int t=a+b;a=b;b=t;}
+    }
+}/**
  * Problem: FibonacciLoop
  * Level: Easy/Mid/Hard progression
  * Includes: problem idea, executable solution, output practice and interview revision.
