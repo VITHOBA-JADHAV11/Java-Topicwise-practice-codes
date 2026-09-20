f15cf778d7cef3847f4f34ee903e8c1d2cff27c9
Vithuu <work.gopal01@gmail.com>
2026-09-20 20:55:43 +0530

    Update EvenOdd.java

--- 01_Basics/Conditionals/EvenOdd.java
+++ 01_Basics/Conditionals/EvenOdd.java
@@ -7,6 +7,7 @@
  */
 public class EvenOdd {
     public static void main(String[] args) {
-        int n=17; System.out.println(n%2==0?"Even":"Odd");
+        int n=17; 
+        System.out.println(n%2==0?"Even":"Odd");
     }
 }
