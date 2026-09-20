8c4b6937197a5ce515e3801b34ef6aabe9087b64
Vithuu <work.gopal01@gmail.com>
2026-09-20 21:07:31 +0530

    Update MultiplicationTable.java

--- 01_Basics/Loops/MultiplicationTable.java
+++ 01_Basics/Loops/MultiplicationTable.java
@@ -1,4 +1,12 @@
 /**
+ * MultiplicationTable: working Java practice program.
+ * Extend this solution with Scanner input, edge-case tests, and complexity analysis.
+ */
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)System.out.println("7 x "+i+" = "+7*i);
+    }
+}/**
  * Problem: MultiplicationTable
  * Level: Easy/Mid/Hard progression
  * Includes: problem idea, executable solution, output practice and interview revision.
